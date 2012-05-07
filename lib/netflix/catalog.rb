68acb9a7ae3514e5ead05bf26c18d45ea276495b
John Barton (joho) <jrbarton@gmail.com>
2012-05-07 19:18:43 +1000

    Change Catalog#download_index to use gzip
    This is optional... and also somewhat theoretical. I kept getting
    timeouts testing it, but Netflix does crap out pretty often so the code
    could be OK.

--- lib/netflix/catalog.rb
+++ lib/netflix/catalog.rb
@@ -4,23 +4,39 @@ require 'oauth'
 
 module Netflix
   class Catalog
+
+    class CatalogDownloadError < StandardError; end
+
     def initialize(oauth_consumer)
       @oauth_consumer = oauth_consumer
     end
 
     # This will download a full index of every title available for instant
     # streaming into a huge XML file. I don't really recommend running it.
-    def download_index(file_path)
+    #
+    # Parameters are:
+    #   * file_path (String)
+    #   * gzipped (boolean - if you want to download it gzipped)
+    #
+    def download_index(file_path, gzipped = false)
+      request_options = gzipped ? {'Accept-Encoding' => 'gzip'} : {}
+
       request = @oauth_consumer.create_signed_request :get,
-                                               'http://api.netflix.com/catalog/titles/index'
-
+                                   'http://api.netflix.com/catalog/titles/index',
+                                   nil,     # oauth token
+                                   {},      # body (for post/put)
+                                   request_options
       
+      @oauth_consumer.http.request(request) do |response|
+        if response.is_a?(Net::HTTPSuccess)
+          file_open_mode = response.header['Content-Encoding'] == 'gzip' ? 'wb' : 'w'
 
-      File.open(file_path, 'w') do |file|
-        @oauth_consumer.http.request(request) do |response|
-           response.read_body do |chunk|
-             file.write(chunk)
-           end
+          File.open(file_path, file_open_mode) do |file|
+            response.read_body(file)
+          end
+        else
+          # TODO: Make informative
+          raise CatalogDownloadError.new
         end
       end
     end
